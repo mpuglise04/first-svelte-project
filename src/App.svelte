4622cf7ca9b0be7efa1cffada7b6e3d1bb274c0d
mpuglise04 <mjpuglise@gmail.com>
2023-06-07 16:23:57 -0400

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+	import Card from './components/Card.svelte';
+</script>
+
+<main>
+	<nav>
+		<a href="https://codeplus.duke.edu/" target="_blank">Code+</a>
+		<a href="https://github.com/mpuglise04" target="_blank">More Code</a>
+		<a href="https://svelte.dev/" target="_blank">Svelte</a>
+	</nav>
+	
+	<h1>Welcome to the Museum!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Card />
+
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	nav {
+		position: fixed;
+  		width: 100%;
+		display: flex;
+		padding: 10px;
+		justify-content: space-around; 
+		z-index: 2;
+		background-color: aliceblue;
+		
+	}
+
+	a:hover {
+		color: greenyellow;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	
+	}
+	
+</style>
---
+++ src/components/Card.svelte
@@ -0,0 +1,41 @@
+<script>
+    // logic goes here
+  </script>
+  
+  <style>
+    #coach-k img{
+        width: 35%;
+    }
+    p {
+        width: 50%;
+        text-align: center;
+        margin-left: auto;
+        margin-right: auto;
+    }
+  </style>
+
+  <main>
+    <div id="coach-k">
+        <img src="https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTg4NTE4Mzc0ODc0MDMyMDc1/coach-k-coaches-duke-unc-final-four.jpg" alt="Coach K"/>
+        <h3>Coach K</h3>
+        <p>Mike Krzyzewski (b.1947): Head coach of Duke University’s men’s basketball team 1980-2022. 
+            He led the team to win five NCAA tournament championships, and has won several gold medals 
+            as the head coach for the US Men’s National Basketball team, 2005-2016.</p>
+    </div>
+    <div id="james-duke">
+        <img src="https://library.duke.edu/sites/default/files/styles/medium/public/2022-02/jb_duke_crop.jpg?itok=t2EE3T0d" alt="James Buchanan Duke"/>
+        <h3>James Buchanan Duke</h3>
+        <p>James Buchanan Duke (1856-1925): Incorporated the American Tobacco Company, which became the largest
+             tobacco conglomerate in the world. Founded the Southern Power Companyy (now Duke Energy). 
+             And established the Duke Endowment, resulting in the transformation of Trinity College into 
+             Duke University</p>
+    </div>
+    <div id="frederick-brooks">
+        <img src="https://ethw.org/w/images/thumb/c/c8/Frederick_P._Brooks%2C_Jr._2400.jpg/300px-Frederick_P._Brooks%2C_Jr._2400.jpg" alt="Frederick Brooks"/>
+        <h3>Frederick Brooks</h3>
+        <p>Frederick P. Brooks (1931-2022): He was instrumental in developing the IBM System 360 computers 
+            and software in the 1970s. He received his Bachelor of Science degree in physics from Duke University 
+            and set up a department of computer and information science at UNC-Chapel Hill in 1964.</p>
+    </div>
+
+</main>
